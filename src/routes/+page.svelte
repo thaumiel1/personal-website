d15fac16fc81267c0db6e479dd178616bb62e2c3
Will Kane <willkane@skiff.com>
2023-12-13 12:02:19 +0000

    Update main page to old repo's main page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let count = 0;
+function increment() {
+  count += 1;
+}
+</script>
+<h1>Hi, welcome to my very simple website at the moment.</h1>
+<button on:click={increment}> {count} </button> 
+<div>
+<p>
+I am a student at Godalming College, studying Physics, Computer Science and Psychology at A level.
+</p>
+</div>
